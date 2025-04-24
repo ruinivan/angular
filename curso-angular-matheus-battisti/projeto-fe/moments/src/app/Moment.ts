da7f2c736526ba02b7075ae54a90c06e559e9a03
Victor <victorruinivanvg@gmail.com>
2025-04-24 17:12:03 -0300

    salvando

--- curso-angular-matheus-battisti/projeto-fe/moments/src/app/Moment.ts
+++ curso-angular-matheus-battisti/projeto-fe/moments/src/app/Moment.ts
@@ -5,5 +5,5 @@ export interface Moment {
   image: string;
   created_at?: string;
   update_at?: string;
-  comments: [{ text: string; username: string }];
+  comments?: [{ text: string; username: string }];
 }
--- curso-angular-matheus-battisti/projeto-fe/moments/src/app/app.config.ts
+++ curso-angular-matheus-battisti/projeto-fe/moments/src/app/app.config.ts
@@ -1,8 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes)],
 };
